17792f78a20e7046de339fbe7ea821b1c8a87260
dishitha <dishitha18@gmail.com>
2026-01-14 10:49:14 +0530

    Changed ip and made other changes to app.jsx etc

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-export const API_BASE = "http://192.168.2.37:5000";
+export const API_BASE = "http://172.20.10.5:5000";
 
 export async function getHealth() {
   const res = await fetch(`${API_BASE}/health`);
